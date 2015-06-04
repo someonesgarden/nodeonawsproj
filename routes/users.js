0e2fbdedfe7fc4e634ea01e7a4eba030dde214d5
daisuke_mac <d@someonesgarden.org>
2015-06-04 19:33:01 +0900

    sadfsadfasdf

--- routes/users.js
+++ routes/users.js
@@ -3,33 +3,6 @@ var router 		= express.Router();
 var model 		= require('./../lib/model/model-users');
 
 
-/* GET user registration. */
-router.get('/register', function(req, res) {
-    if(req.param('username') || req.param('password') || req.param('email')){
-        var email = unescape(req.param('email'));
-        var emailMatch = email.match(/\S+@\S+\.\S+/);
-        if (emailMatch !== null) {
-            var params = {
-                username: req.param('username').toLowerCase(),
-                password: req.param('password'),
-                email: req.param('email').toLowerCase()
-            };
-
-            model.createUser(params, function(err, obj){
-                if(err){
-                    res.status(400).send({error: 'Unable to register'});
-                } else {
-                    res.send(obj);
-                }
-            });
-        } else {
-            res.status(400).send({error: 'Invalid email'});
-        }
-    } else {
-        res.status(400).send({error: 'Missing required field'});
-    }
-});
-
 /* GET users listing. */
 router.get('/', function(req, res) {
     model.getAllUsers(function(err, obj){
@@ -55,6 +28,34 @@ router.get('/user/:user', function(req, res) {
         }
     });
 });
+
+/* GET user registration. */
+router.get('/register', function(req, res) {
+    if(req.param('username') && req.param('password') && req.param('email')){
+        var email = unescape(req.param('email'));
+        var emailMatch = email.match(/\S+@\S+\.\S+/);
+        if (emailMatch !== null) {
+            var params = {
+                username: req.param('username').toLowerCase(),
+                password: req.param('password'),
+                email: req.param('email').toLowerCase()
+            };
+
+            model.createUser(params, function(err, obj){
+                if(err){
+                    res.status(400).send({error: 'Unable to register'});
+                } else {
+                    res.send(obj);
+                }
+            });
+        } else {
+            res.status(400).send({error: 'Invalid email'});
+        }
+    } else {
+        res.status(400).send({error: 'Missing required field'});
+    }
+});
+
 
 /* POST user login. */
 router.post('/login', function(req, res) {
