147fa6ec910e9c875d093b7dfba410e80e7e1b81
daisuke_mac <d@someonesgarden.org>
2015-06-04 13:19:26 +0900

    users.js changed

--- routes/users.js
+++ routes/users.js
@@ -91,4 +91,31 @@ router.post('/register', function(req, res) {
     }
 });
 
+/* POST user registration. */
+router.post('/register', function(req, res) {
+    if(req.param('username') && req.param('password') && req.param('email')){
+        var email = unescape(req.param('email'));
+        var emailMatch = email.match(/\S+@\S+\.\S+/);
+        if (emailMatch !== null) {
+            var params = {
+                username: req.param('username').toLowerCase(),
+                password: req.param('password'),
+                email: req.param('email').toLowerCase()
+            };
+
+            model.createUser(params, function(err, obj){
+                if(err){
+                    res.status(400).send({error: 'Unable to register'});
+                } else {
+                    res.send(obj);
+                }
+            });
+        } else {
+            res.status(400).send({error: 'Invalid email'});
+        }
+    } else {
+        res.status(400).send({error: 'Missing required field'});
+    }
+});
+
 module.exports = router;
