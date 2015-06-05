a9547b50959436e2a1a7486438168077d8fd2e4e
daisuke_mac <d@someonesgarden.org>
2015-06-05 16:09:38 +0900

    set res.header('Cache-Control','no-cache, no-store') on all Routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+    res.header('Cache-Control','no-cache, no-store');
     res.render('index', { title: 'FUNWITHDATA:' });
 });
 
