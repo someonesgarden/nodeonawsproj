63a84dcc6b28964cf621cfd7ec8cd8d09613fb27
daisuke_mac <d@someonesgarden.org>
2015-06-08 14:54:19 +0900

    debugging the S3 confidential variables T/F

--- config/local.js
+++ config/local.js
@@ -9,6 +9,7 @@ module.exports = {
     awsVariables:{
         bucket:'1photoalbums.funwithdata.org',
         key:'AKIAJHNQ3NCHWHDJ7UAA',
-        secret:'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'
+        secret:'kw813YBBD1k+UgdtrCcGtXgTTdGI7wDkHSx6f9Cl',
+        domain:'http://www.funwithdata.org'
     }
 }
